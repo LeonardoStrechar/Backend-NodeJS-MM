475c741809fb4b920c6536e37348b3b357b11e2a
LeonardoStrechar <strechar21@gmail.com>
2022-07-12 20:51:01 -0300

    prisma config 100%

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,15 +3,13 @@ const { prismaClient } = require("../database/prismaClient");
 class UserController {
     async create(request, response){
         const { name, email } = request.body;
-        console.log("chegou");
-        // const user = await prismaClient.user.create({
-        //     data: {
-        //         name,
-        //         email
-        //     }
-        // })
-        const user = await prismaClient.user.findMany()
-        console.log(user)
+        
+        const user = await prismaClient.user.create({
+            data: {
+                name,
+                email
+            }
+        })
 
         return response.json(user);
     }
