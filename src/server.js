5358b0a258c801ba7b73fe59b2aea155398f5e93
LeonardoStrechar <strechar21@gmail.com>
2022-07-14 20:15:27 -0300

    setup cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const { response } = require('express');
 const express = require('express');
+const cors = require('cors');
 const { AppError } = require('./errors/AppError');
 const router = require('./routes');
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
