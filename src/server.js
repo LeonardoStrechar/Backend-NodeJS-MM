5aef74868d2b02ee429a9a10d83082cda0063403
LeonardoStrechar <strechar21@gmail.com>
2022-07-18 02:42:19 -0300

    add swagger documentation

--- src/server.js
+++ src/server.js
@@ -16,6 +16,6 @@ app.use(express.json());
 app.use(router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
 	console.log("Server is running");
 });
